3275c32b529fccf9e3ae85acbcc1ca9aee5caba9
Tyler Beard <tehbearded1@gmail.com>
2022-06-01 18:42:09 -0400

    Updated eda step and deviation code to use new dimensions 224x224

--- step05_EDA.py
+++ step05_EDA.py
@@ -19,7 +19,7 @@ def CSV_To_DF(path):
 def DF_To_Image(df, image_num):
     df = df.iloc[:, 1:] #remove labels column
     image = df.iloc[image_num, :]
-    image = image.to_numpy().reshape(240, 135)
+    image = image.to_numpy().reshape(224, 224)
     return image
 
 def FeatureMeansPerClass(inpath):
@@ -33,7 +33,7 @@ def FeatureMeansPerClass(inpath):
         fig, ax = plt.subplots(1, 3, figsize=(15,10))
         plt.gray()
         ax[0].imshow(DF_To_Image(df, image_num=1), aspect="auto") #plot first image for each class
-        ax[1].imshow(feature_means.to_numpy().reshape(240, 135), aspect="auto") #plot feature means image
+        ax[1].imshow(feature_means.to_numpy().reshape(224, 224), aspect="auto") #plot feature means image
         ax[2].plot(range(len(feature_means)),feature_means)  #plot feature means
         
         #customize appearance
@@ -62,47 +62,47 @@ else:
 
 
 
-# Class Balance Plot
-import os
-import pathlib
-import matplotlib.pyplot as plt
-from collections import OrderedDict
-
-
-#Collect subdir counts into array
-rootdir = "/Users/andrew/PycharmProjects/ASLImageRecognition/gray_frames"
-keys = []
-values = []
-for subdir, dirs, files in os.walk(rootdir):
-    initial_count = 0
-    for path in pathlib.Path(subdir).iterdir():
-        if path.is_file():
-            initial_count += 1
-        subdirClean = os.path.basename(os.path.normpath(subdir))
-        count = initial_count
-    keys.append(subdirClean)
-    values.append(count)
-
-# Convert to Dict
-countDict = {}
-for key, value in zip(keys, values):
-    if key != 'gray_frames':
-        countDict[key] = value
-
-countDict = OrderedDict(sorted(countDict.items()))
-
-print(countDict)
-
-# Visualize
-names = list(countDict.keys())
-values = list(countDict.values())
-mean =  sum(countDict.values()) / len(countDict)
-
-
-plt.bar(range(len(countDict)), values, tick_label=names)
-ax = plt.gca()
-ax.set_ylim([800, 1000])
-plt.xticks(rotation = 45)
-plt.axhline(y=mean, color ='r', linestyle = '-', label = ('Mean Samples: ', int(mean)))
-plt.legend()
-plt.show()
+# # Class Balance Plot
+# import os
+# import pathlib
+# import matplotlib.pyplot as plt
+# from collections import OrderedDict
+#
+#
+# #Collect subdir counts into array
+# rootdir = "/Users/andrew/PycharmProjects/ASLImageRecognition/gray_frames"
+# keys = []
+# values = []
+# for subdir, dirs, files in os.walk(rootdir):
+#     initial_count = 0
+#     for path in pathlib.Path(subdir).iterdir():
+#         if path.is_file():
+#             initial_count += 1
+#         subdirClean = os.path.basename(os.path.normpath(subdir))
+#         count = initial_count
+#     keys.append(subdirClean)
+#     values.append(count)
+#
+# # Convert to Dict
+# countDict = {}
+# for key, value in zip(keys, values):
+#     if key != 'gray_frames':
+#         countDict[key] = value
+#
+# countDict = OrderedDict(sorted(countDict.items()))
+#
+# print(countDict)
+#
+# # Visualize
+# names = list(countDict.keys())
+# values = list(countDict.values())
+# mean =  sum(countDict.values()) / len(countDict)
+#
+#
+# plt.bar(range(len(countDict)), values, tick_label=names)
+# ax = plt.gca()
+# ax.set_ylim([800, 1000])
+# plt.xticks(rotation = 45)
+# plt.axhline(y=mean, color ='r', linestyle = '-', label = ('Mean Samples: ', int(mean)))
+# plt.legend()
+# plt.show()
