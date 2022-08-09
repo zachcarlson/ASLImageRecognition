5bd03d091d0f42e5da0c7049439c3ee580ff3c52
Zach Carlson <zcarlson@umich.edu>
2022-08-09 13:22:28 -0400

    Fixed file mode bug

--- step04_CSVConversionPreProcess.py
+++ step04_CSVConversionPreProcess.py
@@ -6,21 +6,35 @@ import csv
 
 
 def ConvertImagesToCSV(imageDir="", csvDir="",label=0):
+    
+    #Can't find Image Directory
     if not os.path.exists(imageDir):
         print('No image directory found.')
         return
+
+    #Make CSV Directory if it doesn't exist
     os.makedirs(os.path.dirname(csvDir), exist_ok=True)
 
+    #Check there is at least one image in the Image Directory
     numImg = len(os.listdir(imageDir))
     if numImg < 1:
         print('No images in cropped_frames (i.e. RBG image) directory.')
         return
+
+    #Clear CSV file if it already exists, to avoiding appending too much data
+    csv_file_path = csvDir + f"{csvDir[-2:-1]}" + "_img_pixels.csv"
+    if os.path.exists(csv_file_path):
+        #clear file
+        open(csv_file_path, 'w').close()
+
+    #Create CSV File from Images
     for i, infile in enumerate(os.listdir(imageDir)):
 
         try:
+            #Open Image
             img_file = Image.open(imageDir + infile)
 
-            #get the letter
+            #get the letter for saving
             letter = infile[0]
 
             value = np.asarray(img_file.getdata(), dtype=int).reshape((img_file.size[1], img_file.size[0]))
@@ -29,6 +43,8 @@ def ConvertImagesToCSV(imageDir="", csvDir="",label=0):
             with open(csvDir + letter + "_img_pixels.csv", 'a') as f:
                 writer = csv.writer(f)
                 writer.writerow(value)
+            f.close()
+
         except IOError as e:
             print(e, "ERROR - failed to convert '%s'" % infile)
 
