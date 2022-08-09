3fbeb78c9fe498b4f3d1ed310dafb4ad5dc4de58
Zach Carlson <zcarlson@umich.edu>
2022-08-09 13:22:38 -0400

    Minor formatting changes

--- step01_VideoSplitPreProcess.py
+++ step01_VideoSplitPreProcess.py
@@ -2,16 +2,13 @@ import cv2
 import os
 import sys
 
+#Excluding the letters J and Z
 letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
 
 for x in letters:
-
     current_letter = x
-
     dir_name = os.path.dirname(__file__)
-
     filename = os.path.join(dir_name, 'Videos', current_letter + '.mp4')
-
     frame_location = os.path.join(dir_name, 'framed_videos', 'letter_' + current_letter)
     CHECK_FOLDER = os.path.isdir(frame_location)
 
@@ -20,20 +17,13 @@ for x in letters:
         os.makedirs(frame_location)
 
     capture = cv2.VideoCapture(filename)
-
     frameNr = 0
-
     while (True):
-
         success, frame = capture.read()
-
         if success:
             cv2.imwrite(f'{frame_location}/{current_letter}_frame_{frameNr}.jpg', frame)
-
         else:
             break
-
         frameNr = frameNr + 1
         print(frameNr)
-
     capture.release()
